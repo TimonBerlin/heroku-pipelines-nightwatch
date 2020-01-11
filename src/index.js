46cacb54c2d561a466d511a1c9edcd13cb266042
Bomke, Timon <timon.bomke@asideas.de>
2020-01-11 19:08:03 +0100

    init server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 9080;
+
+// Serves some static files
+app.use(express.static(__dirname + '/static'));
+console.log(__dirname + 'static');
+
+let server = app.listen(PORT, () => {
+  console.log(`Listening on port ${ PORT }!`);
+  // Telling the listeners that the server is ready.
+  app.emit('listening', server);
+});
+
+
+module.exports = app;
