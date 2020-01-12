14afeadf8469527b57e6b8cb8e13fd2b8bee9ebc
Bomke, Timon <timon.bomke@asideas.de>
2020-01-12 19:25:49 +0100

    fixed port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+// Heroku will set the port to use as an env var so you can use it. For local development just use any port you want.
 const PORT = process.env.PORT || 9080;
+
+console.log("===Heroku Nightwatch Pipeline Demo===");
 
 // Serves some static files
 app.use(express.static(__dirname + '/static'));
